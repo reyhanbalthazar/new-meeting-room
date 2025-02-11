642c9ad7f844d764c37f37416784522840e96cda
reyhanbalthazar <reyhanbalthazarepsa@gmail.com>
2025-02-11 12:17:58 +0700

    revamp ui meeting room 4.7

--- src/app/components/pages/home/partials/header/header.component.ts
+++ src/app/components/pages/home/partials/header/header.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  constructor() { }
+export class HeaderComponent implements OnInit {
+  isHomePage: boolean = false;
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    // Check if the current route is the Home page
+    this.isHomePage = this.router.url === '/';
+
+    // Listen for route changes
+    this.router.events.subscribe(() => {
+      this.isHomePage = this.router.url === '/';
+    });
+  }
 }
